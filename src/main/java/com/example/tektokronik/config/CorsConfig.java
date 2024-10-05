504c8ae5c4de2243f08d7ba20bd49e22d296fa3d
sevketugurel <ugurelsevket@gmail.com>
2024-10-05 21:56:42 +0300

    security iptal edildi.

--- src/main/java/com/example/tektokronik/config/CorsConfig.java
+++ src/main/java/com/example/tektokronik/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.example.tektokronik.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
